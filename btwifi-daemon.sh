50c54893ea6af00b59ee00b238da6a355ff9f27f
Adrian Robinson <adrian.j.robinson@gmail.com>
2023-02-17 08:45:26 +0000

    Update btwifi-daemon.sh
    = -> ==

--- btwifi-daemon.sh
+++ btwifi-daemon.sh
@@ -45,32 +45,32 @@ check_tool logger
 
 while true; do
 	if [[ $(iwgetid $INTERFACE) && $(iwgetid -r $INTERFACE) == "BTWi-fi" ]]; then
-		if [[ "$DEBUG" = true ]]; then echo "${green}Info: You're connected to 'BTWi-fi' access point${nc}"; fi
+		if [[ "$DEBUG" == 'true' ]]; then echo "${green}Info: You're connected to 'BTWi-fi' access point${nc}"; fi
 		serverResponse=$($WGET --timeout $WGET_TIMEOUT "$SERVER/home")
 		isLoggedIn=$(echo $serverResponse | grep -c "now logged on to BT Wi-Fi")
 		if [[ $isLoggedIn > 0 ]]; then
-			if [[ "$DEBUG" = true ]]; then echo "${green}Info: You're logged in to BTWi-fi!${nc}"; fi
-			if [[ "$DEBUG" = true ]]; then echo "Info: Sleeping for $DELAY_SUCCESS seconds..."; fi
+			if [[ "$DEBUG"  == 'true' ]]; then echo "${green}Info: You're logged in to BTWi-fi!${nc}"; fi
+			if [[ "$DEBUG"  == 'true' ]]; then echo "Info: Sleeping for $DELAY_SUCCESS seconds..."; fi
 			sleep $DELAY_SUCCESS
 		else
-			if [[ "$DEBUG" = true ]]; then echo "${red}Info: You're not logged in to BTWi-fi${nc}"; fi
-		 	if [[ "$DEBUG" = true ]]; then echo "Info: Attempting login"; fi
+			if [[ "$DEBUG"  == 'true' ]]; then echo "${red}Info: You're not logged in to BTWi-fi${nc}"; fi
+		 	if [[ "$DEBUG"  == 'true' ]]; then echo "Info: Attempting login"; fi
 			serverResponse=$($WGET --timeout $WGET_TIMEOUT --post-data "username=$USERNAME&password=$PASSWORD" "$SERVER/tbbLogon")
 			isAuthenticated=$(echo $serverResponse | grep -c "now logged on")
 			if [[ $isAuthenticated > 0 ]]; then
-				if [[ "$DEBUG" = true ]]; then echo "${green}Success: Authenticating to BTWi-fi was successful!${nc}"; fi
-				if [[ "$DEBUG" = true ]]; then logger -t btwifi "Success: Authenticating to BTWi-fi was successful"; fi
-				if [[ "$DEBUG" = true ]]; then echo "Info: Sleeping for $DELAY_SUCCESS seconds..."; fi
+				if [[ "$DEBUG"  == 'true' ]]; then echo "${green}Success: Authenticating to BTWi-fi was successful!${nc}"; fi
+				if [[ "$DEBUG"  == 'true' ]]; then logger -t btwifi "Success: Authenticating to BTWi-fi was successful"; fi
+				if [[ "$DEBUG"  == 'true' ]]; then echo "Info: Sleeping for $DELAY_SUCCESS seconds..."; fi
 				sleep $DELAY_SUCCESS
 			else
-				if [[ "$DEBUG" = true ]]; then echo "${red}Error: Authenticating to BTWi-fi was unsuccessful${nc}"; fi
-				if [[ "$DEBUG" = true ]]; then echo "Info: Sleeping for $DELAY_LOGIN_ATTEMPTS seconds..."; fi
+				if [[ "$DEBUG"  == 'true' ]]; then echo "${red}Error: Authenticating to BTWi-fi was unsuccessful${nc}"; fi
+				if [[ "$DEBUG"  == 'true' ]]; then echo "Info: Sleeping for $DELAY_LOGIN_ATTEMPTS seconds..."; fi
 				sleep $DELAY_LOGIN_ATTEMPTS
 			fi
 		fi
 	else
-		if [[ "$DEBUG" = true ]]; then echo "${red}Error: You're not connected to 'BTWi-fi' access point${nc}"; fi
-		if [[ "$DEBUG" = true ]]; then echo "Info: Sleeping for $DELAY_INTERFACE seconds..."; fi
+		if [[ "$DEBUG"  == 'true' ]]; then echo "${red}Error: You're not connected to 'BTWi-fi' access point${nc}"; fi
+		if [[ "$DEBUG"  == 'true' ]]; then echo "Info: Sleeping for $DELAY_INTERFACE seconds..."; fi
 		sleep $DELAY_INTERFACE
 	fi
 done
